f671abf269e8243597864924b23dea715b293830
Vanya Holovey <golovejvana9@gmail.com>
2025-07-14 22:38:52 +0300

    react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,7 +2,9 @@ export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
 
-    {person.age && <p className="Person__age">I am {person.age}</p>}
+    {person.age !== undefined && (
+      <p className="Person__age">I am {person.age}</p>
+    )}
 
     {person.isMarried ? (
       <p className="Person__partner">
